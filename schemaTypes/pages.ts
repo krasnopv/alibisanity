2a6dfdf623a647300301a0b7df5acd78bdf9d141
krasnopv <krasnopv@gmail.com>
2025-12-02 11:46:11 +0000

    Pages Sub-title update

--- schemaTypes/pages.ts
+++ schemaTypes/pages.ts
@@ -29,6 +29,47 @@ export default defineType({
       description: 'Used for SEO meta description'
     }),
     defineField({
+      name: 'subtitle',
+      title: 'Sub-title',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'H1', value: 'h1'},
+            {title: 'H2', value: 'h2'},
+            {title: 'H3', value: 'h3'},
+            {title: 'H4', value: 'h4'},
+            {title: 'Quote', value: 'blockquote'}
+          ],
+          lists: [
+            {title: 'Bullet', value: 'bullet'},
+            {title: 'Number', value: 'number'}
+          ],
+          marks: {
+            decorators: [
+              {title: 'Strong', value: 'strong'},
+              {title: 'Emphasis', value: 'em'},
+              {title: 'Underline', value: 'underline'},
+              {title: 'Code', value: 'code'}
+            ],
+            annotations: [
+              {
+                title: 'URL',
+                name: 'link',
+                type: 'object',
+                fields: [
+                  { title: 'URL', name: 'href', type: 'url' }
+                ]
+              }
+            ]
+          }
+        }
+      ],
+      description: 'Sub-title with rich text formatting'
+    }),
+    defineField({
       name: 'heroVideo',
       title: 'Hero Video',
       type: 'file',
